8053e8ab8a2563d81233ad68ed0ce59ffe8eb15e
elmerhomero53 <53914081+elmerhomero53@users.noreply.github.com>
2020-03-04 19:45:50 -0600

    Create HT5.py

---
+++ HT5.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  4 19:39:53 2020
+
+@author: Jose
+"""
+
+# -*- coding: cp1252 -*-
+
+#Jose ramos
+#171448
+# chilerismo
+
+import random
+import simpy
+
