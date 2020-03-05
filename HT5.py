70afb6bc14d5e2ea549bbfd275f7ae7b68b5270e
elmerhomero53 <53914081+elmerhomero53@users.noreply.github.com>
2020-03-04 20:24:12 -0600

    funcion simulacion

--- HT5.py
+++ HT5.py
@@ -14,3 +14,26 @@ Created on Wed Mar  4 19:39:53 2020
 
 import random, simpy
 
+def simulation(processName,x,waiting,CPU):
+
+    global wholeProcess 
+ 
+    yield x.timeout(waiting)
+    
+ 
+    ready = x.now
+
+
+    running = random.randint(1, 10)
+    print ('%s  is ready on %f , it also needs %d of RAM to complete process' % (processName,ready,running))
+    
+
+    with CPU.request() as CPUturn:
+        yield CPUturn     
+        yield x.timeout(running) 
+        print ('%s  get out of the CPU %f'% (processName, x.now))
+        
+        
+    totalTime = x.now - ready
+    print ('%s  takes %f'% (processName, totalTime))
+    wholeProcess = wholeProcess + totalTime
